bad9ccb672be05b5643bec9ee2898da5f97e3b4c
Philipp DL7FL <30368381+DL7FL@users.noreply.github.com>
2017-09-23 13:21:13 +0200

    Formatierung

--- senden.py
+++ senden.py
@@ -14,10 +14,9 @@ login = ""  # DAPNET Benutzername
 passwd = ""  # DAPNET Password
 url = 'http://www.hampager.de:8080/calls'  # versenden uebers Internet
 
-
 text = "text"  # Nachrichtentext bis 80 Zeichen  eingeben
 callsign = ""  # Emfaenger Rufzeichen
-txgroup = "dl-he"  # Sendergruppe zB. DL-all für alle Sender in Deutschland
+txgroup = "DL-HE"  # Sendergruppe zB. DL-all für alle Sender in Deutschland
 
 ###############################################################################
 # Funktionen definieren
