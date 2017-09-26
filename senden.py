5d988aa60a39df87e0b3e88b3717d847f9520434
Philipp DL7FL <30368381+DL7FL@users.noreply.github.com>
2017-09-26 08:49:53 +0200

    URL in Kommentar

--- senden.py
+++ senden.py
@@ -15,7 +15,7 @@ passwd = ""  # DAPNET Password
 url = 'http://www.hampager.de:8080/calls'  # versenden uebers Internet http://www.hampager.de:8080/calls
 
 text = "text"  # Nachrichtentext bis 80 Zeichen  eingeben
-callsign = "dl7fl"  # Emfaenger Rufzeichen
+callsign = ["dl7fl"]  # Emfaenger Rufzeichen
 txgroup = "DL-HE"  # Sendergruppe zB. DL-all fuer alle Sender in Deutschland
 
 ###############################################################################
@@ -23,7 +23,7 @@ txgroup = "DL-HE"  # Sendergruppe zB. DL-all fuer alle Sender in Deutschland
 ###############################################################################
 
 def senden(text, callsign, txgroup, login, passwd, url):
-    json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
+    json_string = '''{"text": "''' + text + '''", "callSignNames": ["''' + callsign[0] + '''"], "transmitterGroupNames": ["''' + txgroup + '''"], "emergency": false}'''
     print(json_string)
     response = requests.post(url, data=json_string, auth=HTTPBasicAuth(login, passwd))
     print(response.status_code)
